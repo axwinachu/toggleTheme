8c502633c528ddbc1322a02a17700d7f1fcd81cc
axwinachu <ceaswin146@gmail.com>
2026-01-11 21:23:40 +0530

    Bugupdate

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   }
   return (
     <div style={styles}>
-        <button style={btn} onClick={toggleTheme}>click me!</button>
+        <button style={btn} onClick={toggleTheme}>click me</button>
     </div>
   )
 }
